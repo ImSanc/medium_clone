085b08ef520f2ae12afd072923ea21b59c49f6ae
Sanchit Mishra <sanchit1417@gmail.com>
2025-01-17 22:36:19 +0530

    common folder

---
+++ common/src/zod.ts
@@ -0,0 +1,10 @@
+
+import {z} from "zod"
+
+export const signUpInput = z.object({
+    email : z.string().email(),
+    password : z.string(),
+    name :  z.string().optional()
+})
+
+type SignUpInput = z.infer<typeof signUpInput>;
