ab167a18ca9e2c9ccbb685f9604e704ff18a265d
Sanchit Mishra <sanchit1417@gmail.com>
2025-01-24 01:00:50 +0530

    removed node modules and add src

--- common/src/signin-up.ts
+++ common/src/signin-up.ts
@@ -1,6 +1,14 @@
 
 import {z} from "zod"
 
+export const signIpInput = z.object({
+    email : z.string().email(),
+    password : z.string().min(6)
+})
+
+export type SignInInput = z.infer<typeof signIpInput>;
+
+
 export const signUpInput = z.object({
     email : z.string().email(),
     password : z.string(),
--- common/src/signin.ts
+++
@@ -1,9 +0,0 @@
-
-import {z} from "zod"
-
-export const signIpInput = z.object({
-    email : z.string().email(),
-    password : z.string().min(6)
-})
-
-export type SignIpInput = z.infer<typeof signIpInput>;
