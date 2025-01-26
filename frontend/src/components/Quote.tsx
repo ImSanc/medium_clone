aeeb5c148c3b648142c353fef85ce7c62f053a45
Sanchit Mishra <sanchit1417@gmail.com>
2025-01-26 17:23:28 +0530

    medium frontend quote

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,16 @@
+export const Quote = () =>{
+    return <div className="bg-gray-200 h-screen flex justify-center flex-col px-4 sm:px-10 md:px-14 lg:px-24">
+                <div className=" flex flex-col justify-center text-xl sm:text-2xl  font-semibold text-slate-600 text-left ">
+                "The Customer service I recieved was exceptional. The support team went above and beyond to address my concerns."
+                </div>
+                <div >
+                    <div className="pt-2 font-bold text-slate-800 ">
+                        Sanchit Mishra
+                    </div>
+                    <div className=" text-slate-800 font-thin text-sm">
+                        Dev,Keen learner
+                    </div>
+                </div>
+       
+    </div>
+}
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import { Quote } from "../components/Quote"
+
+export const Signup = () =>{
+    return <div className="  grid grid-cols-2">
+        <div>here</div>
+        <div className=" invisible md:visible">
+            <Quote ></Quote>
+        </div>
+        
+    </div>
+}
