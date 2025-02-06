339c57e5702d2a3d5d2e4a543ca08e9f25bc92c1
Sanchit Mishra <sanchit1417@gmail.com>
2025-02-06 19:46:27 +0530

    Adding signin and up request

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,16 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import InputBox from "./InputBox";
 import { SignUpInput } from "@imsanc/medium-common";
 import { useState } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export const Auth = ( {type} : { type : "signin" | "signup"}) => {
 
     const isSignUp = (type === "signup");
+    const navigate = useNavigate();
     const [postInputs , setPostInputs] = useState<SignUpInput> ({
         name : "",
         email : "",
         password : ""
     });
+
+    async function sendRequest ()
+    {
+        try{
+            const response = await axios.post( `${BACKEND_URL}/api/v1/user/${isSignUp ? "signup" : "signin"}` , postInputs);
+            const jwt = response.data;
+            localStorage.setItem("token", jwt);
+            navigate("/blogs");
+        }
+        catch (e )
+        {
+
+        }
+    }
 
     return <div className="h-screen flex justify-center ">
         <div className=" flex justify-center  flex-col">
@@ -27,12 +44,12 @@ export const Auth = ( {type} : { type : "signin" | "signup"}) => {
             </div>
             <form>
                 <div className=" pt-8">
-                    <InputBox  label="User Name" placeholder="Your name..." onChange={ (e)=>{
+                    {isSignUp ? <InputBox  label="User Name" placeholder="Your name..." onChange={ (e)=>{
                         setPostInputs( c => ({
                             ...c,
                             name : e.target.value
                         }))
-                    }} />
+                    }}  /> : null}
                     <InputBox  label="E-mail" placeholder="your..@email.com" onChange={ (e)=>{
                         setPostInputs( c => ({
                             ...c,
@@ -47,7 +64,7 @@ export const Auth = ( {type} : { type : "signin" | "signup"}) => {
                     }} />
                 </div>
             </form>
-            <button type="button" className=" mt-5 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+            <button onClick={ sendRequest} type="button" className=" mt-5 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                 {isSignUp ? "Sign Up" : "Sign in"}
             </button>
 
