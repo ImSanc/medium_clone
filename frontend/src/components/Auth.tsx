9c30c4fbe229a092a943a95e925e0a15fe6ddb52
Sanchit Mishra <sanchit1417@gmail.com>
2025-02-11 01:15:31 +0530

    authenticate the signup and sign in page

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -16,7 +16,7 @@ export const Auth = ( {type} : { type : "signin" | "signup"}) => {
         password : ""
     });
 
-    //We should always authenticate the token as well authenticate
+    //We should always authenticate the token as well 
     useEffect( ()=>{
         if( localStorage.getItem("token"))
         {
@@ -84,9 +84,7 @@ export const Auth = ( {type} : { type : "signin" | "signup"}) => {
             </form>
             <button onClick={ sendRequest} type="button" className=" mt-5 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                 {isSignUp ? "Sign Up" : "Sign in"}
-            </button>
-
-            
+            </button>    
         </div>
         
     </div>
