de8ce7a3ec4d9f963bb140f3c47518dadc39ecb1
Sanchit Mishra <sanchit1417@gmail.com>
2025-02-12 23:28:50 +0530

    added custom domain to our backend url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "https://backend.sanchitmishra9818779950.workers.dev"
+export const BACKEND_URL = "https://medium.imsanc.com"
--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,6 +1,6 @@
 export default function NotFoundPage() {
     return (
-        <div className="flex justify-center items-center w-screen h-screen bg-slate-900">
+        <div className="flex justify-center items-center w-screen h-screen bg-gray-900">
             <div className="flex  justify-center items-center  w-screen h-screen  sm:w-[80%] sm:h-[80%] px-4 sm:px-2  ">
                 <img src="/404-error-page-vector-design.jpeg"  className=" contrast-50  rounded-3xl  shadow-white shadow-2xl "/>
             </div>
